fd0df8f78a5184025a3105086bdff3bd55d257e1
Shobhit yadav <141415719+iamshobhxt@users.noreply.github.com>
2024-09-29 00:06:35 +0530

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,19 +1,1 @@
-def binary_search(arr, left, right, val):
-    while(left <= right):
-        mid = (left + right)//2
-        if(arr[mid] == val):
-           return mid
-        elif(arr[mid] > val ):
-           right = mid - 1
-        else:
-           left = mid + 1
-    return -1   
 
-data = [1,2,3,4,5,6]
-size = len(data)
-n = 1
-ans = binary_search(data, 0, size-1, n)
-if(ans == -1):
-    print(" index not found")
-else:
-    print(" index found ",ans)  
